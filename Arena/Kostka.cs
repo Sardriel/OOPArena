f4d3502532f19ac5b09c868db51a3737de497594
Sardriel <91335030+Sardriel@users.noreply.github.com>
2021-10-18 08:05:02 +0200

    Add project files.

---
+++ Arena/Kostka.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arena
+{
+    class Kostka
+    {
+        /// <summary>
+        /// Generátor náhodných čísel
+        /// </summary>
+        private Random random;
+        /// <summary>
+        /// Počet stěn kostky
+        /// </summary>
+        private int pocetSten;
+        public Kostka()
+        {
+            pocetSten = 6;
+            random = new Random();
+        }
+        public Kostka(int pocetSten)
+        {
+            this.pocetSten = pocetSten;
+            random = new Random();
+        }
+        /// <summary>
+        /// Hodí kostkou
+        /// </summary>
+        /// <returns>Číslo od 1 do počtu stěn</returns>
+        public int hod()
+        {
+            return random.Next(1, pocetSten + 1);
+        }
+        public int VratPocetSten()
+        {
+            return pocetSten;
+        }
+        /// <summary>
+        /// Vrací textovou reprezentaci kostky
+        /// </summary>
+        /// <returns>Textová reprezentace kostky</returns>
+        public override string ToString()
+        {
+            return string.Format("Kostka s {0} stěnami", pocetSten);
+        }
+    }
+}
---
+++ Arena/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Arena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kostka sestistenna = new Kostka(); 
+            Kostka desetistenna = new Kostka(10);
+            
+            Console.WriteLine(sestistenna);
+            for (int i = 0; i <10; i++)
+                Console.Write(sestistenna.hod() + " ");
+
+            Console.WriteLine("\n\n" + desetistenna);
+            for (int i = 0; i <10;i++)
+                Console.Write(desetistenna.hod() + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
